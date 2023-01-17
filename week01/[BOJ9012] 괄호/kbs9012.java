b275d2ec94ddd0e66fceb80194be4d5de763a28c
bosunKwak <214kbs@gmail.com>
2023-01-17 23:44:49 +0900

    곽보선/ 01.16 / [괄호]

---
+++ week01/[BOJ9012] 괄호/kbs9012.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class kbs9012 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < n; i++) {
+            Stack<Character> stack = new Stack<>();
+            String line = br.readLine();
+            boolean isVPS = true;
+            for (int j = 0; j < line.length(); j++) {
+                if(line.charAt(j)=='('){
+                    stack.push('(');
+                }else{  // ')'        
+                    if(stack.isEmpty())    {
+                        isVPS=false;
+                        break;
+                    }
+                    else stack.pop();            
+                }
+            }
+            if(!isVPS||!stack.isEmpty()) System.out.println("NO");
+            else System.out.println("YES");
+        }
+    }
+}
