8c4715919c71e5fa2a8e7c966ea3fc6306152645
bosunKwak <214kbs@gmail.com>
2023-01-17 23:26:12 +0900

    곽보선/01.15/[스택]

--- week01/[BOJ10828] 스택/kbs10828.java
+++ week01/[BOJ10828] 스택/kbs10828.java
@@ -3,7 +3,7 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class kbs {
+public class kbs10828 {
     static Stack<Integer> stack = new Stack<>();
     public static void main(String[] args) throws Exception {
         
