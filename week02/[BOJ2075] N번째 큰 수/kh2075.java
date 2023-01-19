f8715f0909d7a9e12262cc58b8da13d4f5ea970a
KimHyeon <k97h07o11@gmail.com>
2023-01-19 19:22:16 +0900

    Upload: 김 현 / 01.18 / [N번째 큰 수]

---
+++ week02/[BOJ2075] N번째 큰 수/kh2075.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class kh2075 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                pq.add(Integer.parseInt(st.nextToken()));
+            }
+        }
+
+        for (int i = 1; i < N; i++) pq.remove();
+        bw.write(String.valueOf(pq.remove()));
+
+        bw.flush();
+    }
+}
