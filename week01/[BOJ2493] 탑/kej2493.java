4d4b224ad69b85edc2ea08c4c85c84f951fc28db
jjeoungS2 <95362886+jjeoungS2@users.noreply.github.com>
2023-01-23 00:07:30 +0900

    Upload: 권은정 / 01.23 / [탑]

--- week01/[BOJ2493] 탑/kej2493.java
+++ week01/[BOJ2493] 탑/kej2493.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
-
+// Scanner 사용하면 메모리 초과 뜬다,,,( •́ ̯•｡̀ )
 public class kej2493 {
 
 	public static void main(String[] args) throws Exception{
