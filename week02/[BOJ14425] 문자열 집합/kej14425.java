3b645663c59c63de13e70a57ed4cfa2beb4b5cfc
jjeoungS2 <95362886+jjeoungS2@users.noreply.github.com>
2023-01-27 00:20:55 +0900

    Upload: 권은정/ 01.27 / [문자열 집합]

---
+++ week02/[BOJ14425] 문자열 집합/kej14425.java
@@ -0,0 +1,32 @@
+package 백준문제;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class kej14425 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		Set<String> arr = new HashSet<>();
+		
+		for(int i = 0;i<n;i++) {
+			arr.add(br.readLine());
+		}
+		int result = 0;
+		for(int i = 0;i<m;i++) {
+			String s = br.readLine();
+			if(arr.contains(s)) {
+				result++;
+			}
+		}
+		System.out.println(result);
+	}
+
+}
