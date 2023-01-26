a8e54974399c0781c38a27e7a4f61e7b585f9c2f
jjeoungS2 <95362886+jjeoungS2@users.noreply.github.com>
2023-01-27 00:49:25 +0900

    Upload: 권은정 / 01.27 / [N번째 큰 수]

---
+++ week02/[BOJ2075] N번째 큰 수/kej2075.java
@@ -0,0 +1,31 @@
+package 백준문제;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class kej2075 {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+		for(int i = 0; i<n;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j = 0; j<n;j++) {
+				int a = Integer.parseInt(st.nextToken());
+				pq.add(a);
+			}
+			
+		}
+		
+		for(int i = 0;i<n-1;i++) {
+			pq.poll();
+		}
+		System.out.println(pq.peek());
+	}
+
+}
