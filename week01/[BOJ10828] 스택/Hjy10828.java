efaecb76df7260a400eb712cf64cc46b810290d8
jaeyoung <wodud9515@naver.com>
2023-01-29 05:35:11 +0900

    Upload: 황재영 / 01.29 / [스택]

--- week01/[BOJ10828] 스택/Hjy10828.java
+++ week01/[BOJ10828] 스택/Hjy10828.java
@@ -5,8 +5,8 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Stack;
 
-public class Hjy10828 { 
-	public static void main(String[] args) throws IOException {
+public class hjy10828 { 
+	public static void main(String[] args) throws NumberFormatException, IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
