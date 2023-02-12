0c6ddb915b708c7b04e97ecece01f4a07efc5bab
jaeyoung <wodud9515@naver.com>
2023-02-12 22:35:34 +0900

    Upload: 황재영 / 02.12 / [파일 정리] / 788ms

--- week03/[BOJ20291] 파일 정리/hjy20291.java
+++ week03/[BOJ20291] 파일 정리/hjy20291.java
@@ -1,9 +1,8 @@
-package week03;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.TreeMap;
 
 public class hjy20291 {
@@ -11,13 +10,13 @@ public class hjy20291 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		StringBuilder sb;
+		StringBuilder sb = new StringBuilder();
 		
 		int N = Integer.parseInt(br.readLine());	// 파일의 개수
 		Map<String, Integer> map = new TreeMap<>();
 				
 		for (int i = 0; i < N; i++) {
-			String[] name = br.readLine().split("[.]");		// .을 기준으로 나누기
+			String[] name = br.readLine().split("\\.");		// .을 기준으로 나누기
 			String extension = name[1];		// 확장자 이름
 			if(map.containsKey(extension)) {
 				map.replace(extension, map.get(extension)+1);
@@ -27,8 +26,8 @@ public class hjy20291 {
 		}
 		
 		// 출력
-        for (String key : map.keySet()) {
-            System.out.println(key + " " + map.get(key));
+        for ( Entry entry : map.entrySet()) {
+            sb.append(entry.getKey()).append(" ").append(entry.getValue());
         }
 			
 	}
