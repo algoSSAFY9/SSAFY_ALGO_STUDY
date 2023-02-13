c0a3538e165ed4eb8cb2c04b656f8655895738f2
k97h07o11 <122753635+k97h07o11@users.noreply.github.com>
2023-02-13 16:09:45 +0900

    Upload: 김 현 / 02.07 / [빙고] / 156ms

--- week03/[BOJ2578] 빙고/kh2578.java
+++ week03/[BOJ2578] 빙고/kh2578.java
@@ -1,15 +1,19 @@
+import java.io.*;
 import java.util.HashMap;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
-public class kh2578 {
+public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
 
         HashMap<Integer, Integer> hm = new HashMap<>();
         for (int i = 0; i < 5; i++) {
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 5; j++) {
-                hm.put(sc.nextInt(), i * 5 + j);
+                hm.put(Integer.parseInt(st.nextToken()), i * 5 + j);
             }
         }
 
@@ -17,17 +21,22 @@ public class kh2578 {
         for (int i = 0; i < 2; i++) count[i] = new int[5];
         for (int i = 2; i < 4; i++) count[i] = new int[9];
         int line = 0;
-        for (int i = 1; i <= 25; i++) {
-            int rc = hm.get(sc.nextInt());
-            int r = rc / 5, c = rc % 5;
-            int[] idx = {r, c, r + c, c - r + 4};
-            for (int j = 0; j < 4; j++) {
-                if (++count[j][idx[j]] == 5) line++;
-            }
 
-            if (3 <= line) {
-                System.out.println(i);
-                break;
+        for (int i = 0; i < 5; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 5; j++) {
+                int rc = hm.get(Integer.parseInt(st.nextToken()));
+                int r = rc / 5, c = rc % 5;
+                int[] idx = {r, c, r + c, c - r + 4};
+                for (int k = 0; k < 4; k++) {
+                    if (++count[k][idx[k]] == 5) line++;
+                }
+
+                if (3 <= line) {
+                    bw.write(String.valueOf(i * 5 + j + 1));
+                    bw.flush();
+                    System.exit(0);
+                }
             }
         }
     }
