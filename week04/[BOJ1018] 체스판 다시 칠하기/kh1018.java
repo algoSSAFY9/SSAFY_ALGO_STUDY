261042bcd7080d3ab0ecde0d7077f43ca77d5631
k97h07o11 <122753635+k97h07o11@users.noreply.github.com>
2023-02-14 23:49:07 +0900

    Upload: 김현 / 02.14 / [체스판 다시 칠하기] / 132ms

---
+++ week04/[BOJ1018] 체스판 다시 칠하기/kh1018.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class kh1018 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        char[][] board = new char[N][M];
+        for (int i = 0; i < N; i++) {
+            String tmp = br.readLine();
+            for (int j = 0; j < M; j++) {
+                board[i][j] = tmp.charAt(j);
+            }
+        }
+
+        char[][] chess = {
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'}
+        };
+        int ans = 32;
+        for (int i = 0; i <= N - 8; i++) {
+            for (int j = 0; j <= M - 8; j++) {
+                int count = 0;
+                for (int k = 0; k < 8; k++) {
+                    for (int l = 0; l < 8; l++) {
+                        if (board[i + k][j + l] != chess[k][l]) {
+                            count++;
+                        }
+                    }
+                }
+                ans = Math.min(ans, Math.min(count, 64 - count));
+            }
+        }
+        bw.write(String.valueOf(ans));
+        bw.flush();
+    }
+}
