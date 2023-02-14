b49f82ca32d39b3b17d729bc3e8549909c8af36b
Juwon Hwang <hwang9821@icloud.com>
2023-02-14 09:17:58 +0900

    Upload: 황주원 / 02.14 / [기차가 어둠을 헤치고 은하수를] / 808ms

--- week03/[BOJ15787] 기차가 어둠을 헤치고 은하수를/hjw15787.java
+++ week03/[BOJ15787] 기차가 어둠을 헤치고 은하수를/hjw15787.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.StringTokenizer;
@@ -15,46 +14,37 @@ public class hjw15787 {
 		int m = Integer.parseInt(st.nextToken());
 
 		int[][] str = new int[n+1][21];
-		HashSet<Integer> set = new HashSet<>();
 		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
 
 			int num = Integer.parseInt(st.nextToken());
 			int seat = Integer.parseInt(st.nextToken());
-			set.add(seat);
 			if (num == 1) {
 				int ng = Integer.parseInt(st.nextToken());
-				if(str[seat][ng] == 0) {
-					str[seat][ng] = 1;
-				}
-				
+				str[seat][ng] = 1;
 			} else if (num == 2) {
 				int ng = Integer.parseInt(st.nextToken());
 				str[seat][ng] = 0;
 			} else if (num == 3) {
-				for(int j=20; j>0; j--) {
+				for(int j=20; j>1; j--) {
 					str[seat][j] = str[seat][j-1];
 				}
+				str[seat][1] = 0;
 			} else if (num == 4) {
 				for(int j=1; j<20; j++) {
 					str[seat][j] = str[seat][j+1];
 				}
+				str[seat][20] = 0;
 			}
 		}
 		
 		HashSet<String> res = new HashSet<>();
-		for(int x : set) {
-			String ans = "";
-			for(int i : str[x]) {
-				ans += i;
-			}
-			ans = ans.substring(1, ans.length());
-			res.add(ans);
-		}
 		
-		int chk = 0;
-		if((n - set.size()) > 0) chk++;
-		System.out.println(res.size() + chk);
+		for(int i=1; i<n+1; i++) {
+			res.add(Arrays.toString(str[i]));
+		}
+
+		System.out.println(res.size());
 	}
 
 }
