8a79a131d5da312d494799ad8ab6097cc9c2c2bf
deerKBS <122859252+deerKBS@users.noreply.github.com>
2023-02-21 10:22:24 +0900

    Upload : 구배성 / 02.21 / [N-Queen] / 2580ms

---
+++ week05/[BOJ9663] N-Queen/BS9_9663.java
@@ -0,0 +1,48 @@
+package algoStudy.백트래킹;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BS9_9663 {
+
+	static int count=0 ,N;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		int[][] cnt = new int[4][];
+		cnt[0] = new int[N];
+		cnt[1] = new int[N];
+		cnt[2] = new int[2*N-1]; // ↗  ↘
+		cnt[3] = new int[2*N-1];
+		
+		dfs(0, cnt);
+		
+		System.out.println(count);
+	}
+
+	static void dfs(int n, int[][] select) {
+		if(n==N) {
+			count++;
+			return;
+		}
+		
+		for(int j=0; j<N; j++) {
+			if(select[0][n]!=0 || select[1][j]!=0 || select[2][N-n+j-1]!=0 || select[3][n+j]!=0) {
+				continue;
+			}
+			else {
+				select[0][n]=1;
+				select[1][j]=1;
+				select[2][N-n+j-1]=1;
+				select[3][n+j]=1;
+				select[3][n+j]=1;
+				dfs(n+1, select);
+				select[0][n]=0;
+				select[1][j]=0;
+				select[2][N-n+j-1]=0;
+				select[3][n+j]=0;
+			}
+		}
+	}
+}
