39aefb6d1219023e9822e5b7222071b23673a068
k97h07o11 <122753635+k97h07o11@users.noreply.github.com>
2023-03-08 21:02:31 +0900

    Upload: 김 현 / 03.08 / [ATM] / 132ms

---
+++ week07/[BOJ11399] ATM/kh11399.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] P = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) P[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(P);
+
+        int sum = 0;
+        for (int i = 0; i < N; i++) sum += P[i] * (N - i);
+        bw.write(String.valueOf(sum));
+        bw.flush();
+    }
+}
