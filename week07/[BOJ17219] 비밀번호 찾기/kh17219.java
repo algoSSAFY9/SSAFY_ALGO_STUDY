4b5be3b84547b8c3547489708b0817dfc88c7a5d
k97h07o11 <122753635+k97h07o11@users.noreply.github.com>
2023-03-08 21:04:55 +0900

    Upload: 김 현 / 03.08 / [비밀번호 찾기] / 624ms

--- week07/[BOJ17219] 비밀번호 찾기/kh17219.java
+++ week07/[BOJ17219] 비밀번호 찾기/kh17219.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
-public class Main {
+public class kh17219 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
