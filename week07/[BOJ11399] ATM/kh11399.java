b9804fdb472a4d6e3e29b147dd60dbecdc1ee059
k97h07o11 <122753635+k97h07o11@users.noreply.github.com>
2023-03-08 21:02:56 +0900

    Upload: 김 현 / 03.08 / [ATM] / 132ms

--- week07/[BOJ11399] ATM/kh11399.java
+++ week07/[BOJ11399] ATM/kh11399.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class kh11399 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
