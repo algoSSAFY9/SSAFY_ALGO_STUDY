f8ce71aa32a983543d46099f8e6f5a37d6f03f0d
k97h07o11 <122753635+k97h07o11@users.noreply.github.com>
2023-03-08 21:04:28 +0900

    Upload: 김 현 / 03.08 / [비밀번호 찾기] / 624ms

---
+++ week07/[BOJ17219] 비밀번호 찾기/kh17219.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken());
+
+        HashMap<String, String> hm = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            hm.put(st.nextToken(), st.nextToken());
+        }
+
+        for (int i = 0; i < M; i++) {
+            bw.write(hm.get(br.readLine()));
+            bw.newLine();
+        }
+        bw.flush();
+    }
+}
