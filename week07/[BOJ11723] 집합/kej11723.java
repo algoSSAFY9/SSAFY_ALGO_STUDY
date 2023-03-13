0c762199bf29285281f463af0051314d4307356b
jjeoungS2 <95362886+jjeoungS2@users.noreply.github.com>
2023-03-13 16:12:26 +0900

    Upload: 권은정 / 03.13 / [집합] / 1176ms

--- week07/[BOJ11723] 집합/kej11723.java
+++ week07/[BOJ11723] 집합/kej11723.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 import java.util.TreeSet;
 
-public class BJ_11723_ÁıÇÕ {
+public class BJ_11723_ÃÃ½Ã‡Ã• {
 
 	public static void main(String[] args) throws Exception {
 		TreeSet<Integer> set = new TreeSet<>();
@@ -46,7 +46,7 @@ public class BJ_11723_ÁıÇÕ {
 				break;
 			}
 		}
-		System.out.println(sb);
+		System.out.println(sb); 
 	}
 
 }
