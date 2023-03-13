e583b060b0567c3a5de03b1046bed46fe1f70d12
jjeoungS2 <95362886+jjeoungS2@users.noreply.github.com>
2023-03-13 17:54:56 +0900

    Upload: 권은정 / 03.13 / [리모컨] / 364ms

--- week07/[BOJ1107] 리모컨/kej1107.java
+++ week07/[BOJ1107] 리모컨/kej1107.java
@@ -1,5 +1,3 @@
-package BJ;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
@@ -7,39 +5,36 @@ import java.util.StringTokenizer;
 public class BJ_1107_리모컨 {
 	static int N, M;
 	static int[] num = new int[10];
+	static int min;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		M = Integer.parseInt(br.readLine());
-
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < M; i++) {
-			num[Integer.parseInt(st.nextToken())] = -1;
+		if (M != 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < M; i++) {
+				num[Integer.parseInt(st.nextToken())] = -1; // 고장난 숫자면 -1
+			}
 		}
-
+		min = Math.abs(N - 100); // 시작점이 100이니까 초기값은 N-100
 		boolean flag = true;
 		for (int i = 0; i <= 999999; i++) { // 그냥 다 돌아볼까..?
 			String s = String.valueOf(i);
-
+			flag = true;
 			for (int j = 0; j < s.length(); j++) {
 				for (int z = 0; z < num.length; z++) {
-					if (num[z] == s.charAt(j)) {
+					if (num[s.charAt(j) - '0'] == -1) { // 고장난 숫자가 포함되었을 때 넘어가기
 						flag = false;
 						break;
 					}
 				}
 			}
-
-			int cnt = 0;
 			if (flag) {
-				for (int j = i; j <= N; j++) {
-					cnt++;
-				}
+				int cnt = Math.abs(N - i) + s.length();
+				min = Math.min(cnt, min);
 			}
-			
-
 		}
+		System.out.println(min);
 	}
-
 }
