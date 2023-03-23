7abe2cbdf43f7d4cb12c06e8a4daeec6a82f0494
Juwon Hwang <hwang9821@icloud.com>
2023-03-23 11:12:41 +0900

    Upload: 황주원 / 03.23 / [2xn 타일링] / 128ms

---
+++ week09/[BOJ11726] 2xn 타일링/hjw11726.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class hjw11726 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		long[] dp = new long[n+1];
+		
+		dp[1] = 1;
+		
+		if(n >= 2)	dp[2] = 2;
+		
+		for(int i=3; i<=n; i++) {
+			dp[i] = (dp[i-2] + dp[i-1]) % 10007;
+		}
+		
+		System.out.println(dp[n]);
+	}
+}
