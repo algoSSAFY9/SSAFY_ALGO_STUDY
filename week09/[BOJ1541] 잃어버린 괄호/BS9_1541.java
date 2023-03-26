ab8bc41519fc0b78153b567d2c328a7389a74eb1
user <user@DESKTOP-RLIOV8R>
2023-03-26 11:28:42 +0900

    Upload: 구배성 / 03.26 / [잃어버린 괄호] / 124ms

---
+++ week09/[BOJ1541] 잃어버린 괄호/BS9_1541.java
@@ -0,0 +1,33 @@
+package algorithm_study.week9;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BS9_1541 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String string = br.readLine();
+		
+		String[] str = string.split("[-+]");
+		
+		int cnt=0, minus=str.length;;
+		for(int i=0; i<string.length(); i++) {
+			if(string.charAt(i)=='+') cnt++;
+			else if(string.charAt(i)=='-') {
+				minus=cnt+1;
+				break;
+			}
+		}
+		
+		int ans=0;
+		for(int i=0; i<minus; i++) {
+			ans+=Integer.parseInt(str[i]);
+		}
+		for(int i=minus; i<str.length; i++) {
+			ans-=Integer.parseInt(str[i]);
+		}
+		System.out.println(ans);
+	}
+}
