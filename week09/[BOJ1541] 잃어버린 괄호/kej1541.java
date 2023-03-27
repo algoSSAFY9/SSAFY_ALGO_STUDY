6bd124e359f3e3c081fbbe4ca967c6d133078104
jjeoungS2 <95362886+jjeoungS2@users.noreply.github.com>
2023-03-27 11:19:52 +0900

    Upload: 권은정 / 03.26 / [잃어버린 괄호] / 128ms

---
+++ week09/[BOJ1541] 잃어버린 괄호/kej1541.java
@@ -0,0 +1,39 @@
+package algo;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+//읽어버린 괄호
+public class kej1541 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), "-");
+		List<String> list = new ArrayList<>();
+		String s;
+		while (st.hasMoreTokens()) {
+			s = st.nextToken();
+			list.add(s);
+		}
+		int num = Integer.MAX_VALUE;
+		int n = 0;
+		int sum = 0;
+		for (int i = 0; i < list.size(); i++) {
+			sum = 0;
+
+			st = new StringTokenizer(list.get(i), "+");
+			while (st.hasMoreTokens()) {
+				n = Integer.parseInt(st.nextToken());
+				sum += n;
+			}
+			if (num == Integer.MAX_VALUE)
+				num = sum;
+			else {
+				num -= sum;
+			}
+		}
+		System.out.println(num);
+	}
+}
